7b37f04ff1bf409162c61765040425cc9dc98177
Gustavo <gustavo.tenorio@uab.uncisal.edu.br>
2025-09-24 16:27:26 -0300

    commit inicial:cicd-teste

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('JEFERSON GOSTOSAO');
+  res.send('Gusta GOSTOSAO');
 });
 
 app.listen(PORT, HOST, () => {
